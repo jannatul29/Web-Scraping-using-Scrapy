4bd6246e2b9bb73842f89f187024f67b8a9c5a5e
jannatul29 <2017-2-60-029@std.ewubd.bd>
2022-01-10 18:53:38 +0600

    final

--- myproject/myproject/spiders/scrapy.py
+++ myproject/myproject/spiders/scrapy.py
@@ -12,12 +12,9 @@ class QuotesSpider(scrapy.Spider):
         item = 1
         conn = psycopg2.connect(dbname="store", user="postgres", password="root", host="localhost")
         c = conn.cursor()
-        c.execute("CREATE TABLE test11 (id serial PRIMARY KEY, title varchar, herf varchar, image varchar, rating varchar, price varchar, location varchar, amenities text);")
-        # conn.commit()
+        c.execute("CREATE TABLE test12 (id serial PRIMARY KEY, title varchar, herf varchar, image varchar, rating varchar, price varchar, location varchar, amenities text);")
         for quote in response.css('div.soom'):
             if item < 12:
-                # conn = psycopg2.connect(dbname="store", user="postgres", password="root", host="localhost")
-                # c = conn.cursor()
                 title = str(quote.xpath('div/div/div/a/span/text()').get())
                 herf = 'https://www.kayak.co.in' + str(quote.css('a::attr(href)').get())
                 rating = str(quote.xpath('div/div/div/div/span/text()').get())
@@ -26,18 +23,14 @@ class QuotesSpider(scrapy.Spider):
                 amenities = str(quote.css('div.soom-content-wrapper div.soom-freebies-section div.soom-freebies div.soom-freebie span.yRv1-text::text').getall())
                 img = quote.xpath('a/img/@src').get()
                 if img is not None:
-                    c.execute("INSERT INTO test11 (title, herf, image, rating, price, location, amenities) VALUES (%s, %s, %s, %s, %s, %s, %s)", (title, herf, img, rating, price, location, amenities))
+                    c.execute("INSERT INTO test12 (title, herf, image, rating, price, location, amenities) VALUES (%s, %s, %s, %s, %s, %s, %s)", (title, herf, img, rating, price, location, amenities))
                     conn.commit()
-                    # c.close()
-                    # conn.close()
                 elif img is None:
                     imgjson = response.xpath(f"(//script[contains(text(),'{title}')])/text()").getall()
                     data = json.loads(imgjson[0])
                     img = data.get('image')
 
                     if img:
-                        c.execute("INSERT INTO test11 (title, herf, image, rating, price, location, amenities) VALUES (%s, %s, %s, %s, %s, %s, %s)",(title, herf, img, rating, price, location, amenities))
+                        c.execute("INSERT INTO test12 (title, herf, image, rating, price, location, amenities) VALUES (%s, %s, %s, %s, %s, %s, %s)",(title, herf, img, rating, price, location, amenities))
                         conn.commit()
-                        # c.close()
-                        # conn.close()
                 item = item + 1
