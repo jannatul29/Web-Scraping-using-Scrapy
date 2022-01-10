ab90ece2459c29af22a063c6cf0f45ea326e44a3
jannatul29 <2017-2-60-029@std.ewubd.bd>
2022-01-10 18:14:27 +0600

    first commit

---
+++ myproject/myproject/spiders/scrapy.py
@@ -0,0 +1,50 @@
+import scrapy
+import psycopg2
+import json
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = [
+        'https://www.kayak.co.in/London-Hotels.28501.hotel.ksp'
+    ]
+
+    def parse(self, response):
+        item = 1
+        conn = psycopg2.connect(dbname="store", user="postgres", password="root", host="localhost")
+        c = conn.cursor()
+        #c.execute("CREATE TABLE test10 (id serial PRIMARY KEY, title varchar, herf varchar, image varchar, rating varchar, price varchar, location varchar, amenities text);")
+        #conn.commit()
+        for quote in response.css('div.soom'):
+            if item < 12:
+                conn = psycopg2.connect(dbname="store", user="postgres", password="root", host="localhost")
+                c = conn.cursor()
+                # c.execute("CREATE TABLE test6 (id serial PRIMARY KEY, title varchar, herf varchar, image varchar, rating varchar, price varchar, location varchar, amenities text);")
+                # conn.commit()
+                title = str(quote.xpath('div/div/div/a/span/text()').get())
+                herf = 'https://www.kayak.co.in' + str(quote.css('a::attr(href)').get())
+                rating = str(quote.xpath('div/div/div/div/span/text()').get())
+                price = str(quote.xpath('div/div/span/text()').get())
+                location = str(quote.css('div.soom-content-wrapper div.soom-description-wrapper div.soom-description div.soom-price-section span.soom-neighborhood::text').get())
+                amenities = str(quote.css('div.soom-content-wrapper div.soom-freebies-section div.soom-freebies div.soom-freebie span.yRv1-text::text').getall())
+                img = quote.xpath('a/img/@src').get()
+                if img is not None:
+                    c.execute("INSERT INTO test10 (title, herf, image, rating, price, location, amenities) VALUES (%s, %s, %s, %s, %s, %s, %s)", (title, herf, img, rating, price, location, amenities))
+                    conn.commit()
+                    c.close()
+                    conn.close()
+                elif img is None:
+                    imgjson = response.xpath(f"(//script[contains(text(),'{title}')])/text()").getall()
+                    data = json.loads(imgjson[0])
+                    img = data.get('image')
+
+                    # dic = json.loads(response.css('html head script::text').get())
+                    # # img = str(dic["image"])
+                    # img = dic["image"]
+                    if img:
+                        c.execute("INSERT INTO test10 (title, herf, image, rating, price, location, amenities) VALUES (%s, %s, %s, %s, %s, %s, %s)",(title, herf, img, rating, price, location, amenities))
+                        conn.commit()
+                        c.close()
+                        conn.close()
+                item = item + 1
+
+
